fed9597fd024e4a9ab146ddb4827307f07c6db30
Juan-ita <kavatajuanita@gmail.com>
2026-06-20 10:03:32 +0300

    feat: add all html files into one folder

--- folder.js/habit.js
+++ folder.js/habit.js
@@ -1,21 +1,21 @@
-const hbabit_input = document.getElementById("habitInput")
+const habit_input = document.getElementById("habitInput")
 const add_button = document.getElementById("addHabitBtn")
 const habit_list = document.getElementById("habitList")
 
 //Load habits
-const habits = JSON.parse(localStorage.getItem("habits")) || [];
+const habits = JSON.parse(localStorage.getItem("habits")) || []; // gets the saved habits and if nothing is saved, creat an empty array.
 
 //Save habits
 function saveHabits(){
-    localStorage.setItem("habits", JSON.stringify(habits));
+    localStorage.setItem("habits", JSON.stringify(habits)); //Changes the array ito text then stores the text in localStorage
 }
 
 //Render habits
 function renderHabits(){
-    habit_list.innerHTML = "",
+    habit_list.innerHTML = "";
 
     habits.forEach((habit, index) => {
-        const row = document.createElement("div");
+        const row = document.createElement("div");// This creates a new div element
 
         row.innerHTML = `
         <div class="grid grid-cols-8 items-center bg-pink-50 border border-pink-200 p-3 rounded-xl">
@@ -32,27 +32,27 @@ function renderHabits(){
         <button onclick="removeHabit(${index})" class="text-red-500 text-sm mt-2 hover:underline">Delete</button>
         `;
 
-        habit_list.appendChild(row);
+        habit_list.appendChild(row);// Adds the newly created row into the habit list
     });
 
     add_button.onclick = function(){
-        if(hbabit_input.value !== "") {
+        if(habit_input.value !== "") { //Makes sure the user typed sth
             const new_habit = {
-                name: hbabit_input.value
+                name: habit_input.value
             };
 
-            habits.push(new_habit);
-            saveHabits();
-            renderHabits();
+            habits.push(new_habit);// Adds the new habit to the end of the array
+            saveHabits();// Saves the new habit in the localStorage
+            renderHabits();//Show the updated list
 
-            hbabit_input.value = "";
+            habit_input.value = "";//Removes the text after adding the habit
         }
     };
 
-    window.removeHabit = function(position){
-        habits.splice(position, 1);
-        saveHabits();
-        renderHabits();
+    window.removeHabit = function(position){ 
+        habits.splice(position, 1);//Removes one item from the array
+        saveHabits();//Saves the updated array
+        renderHabits(); // Refreshes the page.
     }
 }
-renderHabits()
+renderHabits()//Runs the function when the page opens and the previous saved habits appear automaticlly
